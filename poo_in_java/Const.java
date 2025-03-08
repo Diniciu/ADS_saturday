8e67405e4de15f94c2f6d41718434db34928716e
Vinícius Ferreira <107073278+Diniciu@users.noreply.github.com>
2025-03-08 15:54:32 +0000

    salvar

---
+++ poo_in_java/Const.java
@@ -0,0 +1,16 @@
+public class Const {
+    public static void main(String[] args) {
+        
+        int a = 5; 
+        int b = 7;
+        int maior;
+
+        if (a > b) {
+            maior = a;
+        } else {
+            maior = b;
+        }
+
+        System.out.println("Maior número: " + maior);
+    }
+}
---
+++ poo_in_java/Varis_.java
@@ -0,0 +1,16 @@
+public class Varis_ {
+    public static void main(String[] args) {
+        String nome1 = "Josef";
+        String nome2 = "Stalin";
+        int idade = 54;
+        double peso = 86.12;
+        
+        // CONSTANTE
+        final String CPF = "009.876.543-21";
+
+        System.out.println("Nome: " + nome1 + " " + nome2);
+        System.out.println("Idade: " + idade);
+        System.out.println("Peso: " + peso);
+        System.out.println("CPF: " + CPF);
+    }
+}
